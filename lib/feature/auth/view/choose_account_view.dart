59d4e32b97b69dc19f714e8a2a6a8a1440252c1f
sr_sumanroy <sumantachtg@gmail.com>
2025-12-29 11:06:53 -0800

    choose account type screen

---
+++ lib/feature/auth/view/choose_account_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChooseAccountView extends StatelessWidget {
+  const ChooseAccountView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('Choose Account View')));
+  }
+}
